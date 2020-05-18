e707ac1ce7584b1b27f3180daed00f901498b801
Brent Keller <brentkeller@gmail.com>
2020-05-17 22:26:12 -0400

    Add documentation about go script install
    Also some TODOs

--- src/commands/go/index.ts
+++ src/commands/go/index.ts
@@ -2,6 +2,26 @@ import { flags } from '@oclif/command';
 import { cli } from 'cli-ux';
 import Command from '../../command-base';
 
+/*
+Requires a shell-level script to navigate the working directory to the shortcut.
+
+Installation:
+
+PowerShell
+TODO: Add goto.ps1 content here?
+1. Create a script (see c:\dev\scripts\goto.ps1)
+2. Add the script with an alias to the user's profile:
+- C:\Users\brent\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+or 
+- C:\Users\brent\Documents\WindowsPowerShell\profile.ps1
+2a.
+    # GO (requires bkcli installed or linked (npm link))
+    function Go-To-Shortcut { c:\dev\scripts\goto.ps1 $args }
+    Set-Alias go Go-To-Shortcut
+3. Use the shortcut: `go <shortcut name>` (e.g. `go dev`)
+
+*/
+
 export default class Go extends Command {
   static description = 'create shortcuts and jump to directories by name';
 
@@ -36,6 +56,8 @@ go to shortcut named 'name'
 
   listShortcuts(data: any) {
     const entries = Object.entries(data.shortcuts);
+    // TODO: Alphabetize by shortcut name
+    // TODO: Allow filtering by name or path?
     if (entries.length === 0) {
       this.log('No shortcuts saved');
       return;
